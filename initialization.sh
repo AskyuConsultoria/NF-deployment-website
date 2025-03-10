08de0c00023767b32e8ecbb4b321fb99f2f30e2f
kayky <kayky.pedroso@sptech.school>
2025-03-09 22:34:05 -0300

    Fix(initialization.sh): Possível erro de compatibilidade

--- initialization.sh
+++ initialization.sh
@@ -5,5 +5,6 @@ git clone https://github.com/AskyuConsultoria/Gestio-front-api.git
 
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-sudo docker build -t website-image -f Gestio-deployment-website/install_website.dockerfile .
+sudo docker build -t website-image -f Gestio-deployment-website/install_website>
 sudo docker run --name website-container -d -p 80:80 website-image
+
