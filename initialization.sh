2a3a0d8ba721a9f7ea28adc90b5c742951ede454
Kayky <125297368+Kaykybelarmino@users.noreply.github.com>
2025-03-09 21:52:20 -0300

    Fix(Initialization.sh): Instalação do docker de outra forma

--- initialization.sh
+++ initialization.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-sudo apt-get update
-
-
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-
-sudo apt install docker.io -y
+sudo apt update -y
+sudo apt upgrade -y
 git clone https://github.com/AskyuConsultoria/Gestio-front-api.git
-sudo docker build image -t website-image -f ~/Gestio-deployment-website/install_website.dockerfile .
-sudo docker run --name website-container -d -p 80:80 website-image 
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo docker build -t website-image -f Gestio-deployment-website/install_website.dockerfile .
+sudo docker run --name website-container -d -p 80:80 website-image
